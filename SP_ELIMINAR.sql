2f5b38b14d97732d3d068414f84cbe832d40751c
tati <tatimorales02@icloud.com>
2020-05-30 10:19:01 -0600

    eliminar Pinches

--- SP_ELIMINAR.sql
+++ SP_ELIMINAR.sql
@@ -1,23 +1,3 @@
--------------------------------------------------Eliminar Cocinero------------------------------------------------------------------
-USE CIELOAZUL
-GO
-CREATE PROC EliminarCocinero (@IdCocinero INT)
-AS
-	IF (@IdCocinero = '') 
-		BEGIN
-			PRINT 'EL ID DE COCINERO NO PUEDE SER VACIO'
-		END
-	ELSE
-		BEGIN
-		    DELETE FROM Pinches WHERE Pinches.IdCocinero = @IdCocinero
-			PRINT 'SE HA ELIMINADO EL PINCHE'
-			DELETE FROM Cocineros WHERE Cocineros.IdCocinero = @IdCocinero
-			PRINT 'SE HA ELIMINADO EN COCINERO'
-	END
-EXEC EliminarCocinero '1'
-
-SELECT IdCocinero, AnniosServicio, IdEmpleado FROM Cocineros
-
 ---------------------------------------Eliminar Empleado ----------------------------------
 	GO
 	USE CIELOAZUL
@@ -42,3 +22,52 @@ SELECT IdCocinero, AnniosServicio, IdEmpleado FROM Cocineros
 			PRINT 'SE HA ELIMINADO EL EMPLEADO'
 	END
 	EXEC EliminarEmpleado '505050505'
+
+
+
+-------------------------------------------------Eliminar Cocinero------------------------------------------------------------------
+USE CIELOAZUL
+GO
+CREATE PROC EliminarCocinero (@IdCocinero INT)
+AS
+	IF (@IdCocinero = '') 
+		BEGIN
+			PRINT 'EL ID DE COCINERO NO PUEDE SER VACIO'
+		END
+	ELSE
+		BEGIN
+		    DELETE FROM Pinches WHERE Pinches.IdCocinero = @IdCocinero
+			PRINT 'SE HA ELIMINADO EL PINCHE'
+			DELETE FROM Cocineros WHERE Cocineros.IdCocinero = @IdCocinero
+			PRINT 'SE HA ELIMINADO EN COCINERO'
+	END
+EXEC EliminarCocinero '1'
+
+SELECT IdCocinero, AnniosServicio, IdEmpleado FROM Cocineros
+
+------------------------------------------------EliminarPinches----------------------------------------------------
+
+USE CIELOAZUL
+GO
+CREATE PROC EliminaPinches (@IdPinche varchar(8)
+                          
+)
+AS
+		IF ((@IdPinche = '')  )
+		BEGIN
+			PRINT 'NO SE PERMITEN CAMPOS VACIOS'
+		END
+
+		ELSE
+   
+		BEGIN
+		    DELETE FROM Empleados WHERE Empleados.Dni = @IdPinche
+			PRINT 'SE HA ELIMINADO CORRECTAMENTE EL EMPLEADO'
+			DELETE FROM Pinches WHERE Pinches.IdPinche = @IdPinche
+			PRINT 'SE HA ELIMINADO EL COCINERO'
+	
+	END
+		
+EXEC EliminaPinches 3
+
+SELECT IdPinche,IdCocinero,FechaNacimiento,IdEmpleado  FROM Pinches
